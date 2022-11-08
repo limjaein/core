d145b68bcca1f43334711dfd0ffc3645323bb781
limjaein <dlawodls96@naver.com>
2022-11-08 21:54:02 +0900

    주문과 할인 도메인 설계 및 개발

--- src/main/java/hello/core/member/MemoryMemberRepository.java
+++ src/main/java/hello/core/member/MemoryMemberRepository.java
@@ -5,9 +5,9 @@ import java.util.Map;
 
 public class MemoryMemberRepository implements MemberRepository{
 
-    /*
-        실무에서는 동시성 이슈때문에 HashMap 대신 ConcurrentHashMap 을 사용하는 게 좋다.
-        예시에서는 HashMap으로 진행해보자.
+    /**
+     * 실무에서는 동시성 이슈때문에 HashMap 대신 ConcurrentHashMap 을 사용하는 게 좋다.
+     * 예시에서는 HashMap으로 진행해보자.
      */
     private static Map<Long, Member> store = new HashMap<>();
 
